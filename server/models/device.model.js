7801241f4ff58d0c83a80dbada05007bc5cd862d
RH-Kevin <147596439+RH-Kevin@users.noreply.github.com>
2024-02-18 19:11:14 +0900

    save

--- server/models/device.model.js
+++ server/models/device.model.js
@@ -7,17 +7,17 @@ module.exports = {
     DEVICE_TABLE,
 
     getAll(limit = 5) {
-        return knex
-            .select({
-                serial_number: "serial_number",
-                first_name: "first_name",
-                last_name: "last_name",
-                date_assigned: "date_assigned",
-                warranty: "warranty_end",
-                status: "status",
-                location: "location"
-            })
-            .from(DEVICE_TABLE)
-            .limit(limit);
+        return knex('devices').select('*');
+            // .select({
+            //     serial_number: "serial_number",
+            //     first_name: "first_name",
+            //     last_name: "last_name",
+            //     date_assigned: "date_assigned",
+            //     warranty: "warranty_end",
+            //     status: "status",
+            //     location: "location"
+            // })
+            // .from(DEVICE_TABLE)
+            // .limit(limit);
     }
 }
