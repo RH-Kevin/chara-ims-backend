58196770af2de8fe8f8e17246798aa50abff0821
RH-Kevin <147596439+RH-Kevin@users.noreply.github.com>
2024-02-19 15:57:27 +0900

    config: seeds & migrations in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -46,7 +46,10 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './db/data/migrations'
+    },
+    seeds: {
+      directory: './db/data/seeds'
     },
   }
 
